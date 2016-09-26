8ab16ac3d87f91cc23e03a62953bb9f6ad953a2d
AdiJNair <adijnair@gmail.com>
2016-09-26 18:11:14 +1300

    created character and first map + spider

---
+++ Fading Light/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+
+    // Handling
+    public float rotationSpeed = 20000;
+    public float walkSpeed = .0000002f;
+    public TextMesh levelText;
+    public bool dead = false;
+
+    // System
+    private Quaternion targetRotation;
+
+    // Components
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        //ControlMouse();
+       
+        ControlWASD();
+        //transform.Rotate(Vector3.up * Time.deltaTime * 1000);
+       
+    }
+
+
+
+    void ControlWASD()
+    {
+        Vector3 input = new Vector3(-Input.GetAxisRaw("Horizontal"), 0, -Input.GetAxisRaw("Vertical"));
+
+        if (input != Vector3.zero)
+        {
+            targetRotation = Quaternion.LookRotation(input);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+        }
+
+        Vector3 motion = input;
+        //motion *= (-Mathf.Abs(input.x) == 1 && -Mathf.Abs(input.z) == 1) ? .7f : 1;
+        motion.x = motion.x * walkSpeed;
+        motion.z = motion.z * walkSpeed;
+        //motion *=  walkSpeed;
+       // motion += Vector3.up * -8;
+
+        controller.Move(motion * Time.deltaTime);
+    }
+
+
+}
