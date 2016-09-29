6a57cfda0e5a9b255581285fdc2ad51da354a585
Ammar Bagasrawala <ammarb27@gmail.com>
2016-09-29 18:24:00 +1300

    Added pressure plate and animated wall falling over

---
+++ Fading Light/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		GameObject wall = GameObject.FindWithTag ("Falling Wall");
+		wall.GetComponent<FallingWall>().pressurePlate1 = true;
+		wall.GetComponent<FallingWall>().pressurePlate2 = true;
+	}
+
+	void OnTriggerLeave(Collider other) {
+		GameObject wall = GameObject.FindWithTag ("Falling Wall");
+		wall.GetComponent<FallingWall>().pressurePlate1 = false;
+		wall.GetComponent<FallingWall>().pressurePlate2 = true;
+	}
+}
