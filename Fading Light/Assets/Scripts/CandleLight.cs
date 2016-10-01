0300aac3905bf9c6132172b77ec6a5c064989207
AdiJNair <adijnair@gmail.com>
2016-10-01 15:59:33 +1300

    adding candle lights

---
+++ Fading Light/Assets/Scripts/CandleLight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandleLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // When collision occurs between two objects
+    void OnTriggerStay(Collider other)
+    {
+
+        // Checking if players are next to each other
+        if (other.gameObject.tag.Equals("Player2") || other.gameObject.tag.Equals("Player1"))
+        {
+            Debug.Log("Hit candle");
+            // Checking if users wanted to swap the torch
+            if (Input.GetButtonDown("CandleLight"))
+            {
+                
+            }
+        }
+    }
+}
