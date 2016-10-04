aad6f134efe41fbe8e86cd5d709028526450ae90
plokji <okplokjiloop@hotmail.com>
2016-10-04 23:17:59 +1300

    imported the custom lever, and implemented the moving walls

---
+++ Fading Light/Assets/lever.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lever : MonoBehaviour
+{
+    private bool _pulled = false;
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.T) && !_pulled)
+        {
+            this.GetComponent<Animation>().Play("LeverDown");
+            GameObject rightMovingWall = GameObject.FindGameObjectWithTag("Right Moving Wall");
+
+            rightMovingWall.GetComponent<Animation>().Play("RightMovingWallOut");
+
+            GameObject leftMovingWall = GameObject.FindGameObjectWithTag("Left Moving Wall");
+
+            leftMovingWall.GetComponent<Animation>().Play("LeftMovingWallOut");
+            _pulled = true;
+        }
+    }
+}
