5055b6459afba49e2e4b27ceba49c09d042fa592
unknown <nathansitu@hotmail.com>
2016-10-06 12:14:28 +1300

    Created ListItem Controller

--- Fading Light/Assets/Scripts/ListItemController.cs
+++ Fading Light/Assets/Scripts/ListItemController.cs
@@ -1,15 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ListItemController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public Image Icon;
+	public Text Name, Description;
+	public int RewardPoints
+
 }
