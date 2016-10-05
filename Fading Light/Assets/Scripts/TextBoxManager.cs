ff136edfaab2fa5954d6246e253a037780192396
Jun Hao Xu <junhaoxu13@hotmail.com>
2016-10-05 14:40:32 +1300

    Refactored code and updated references

--- Fading Light/Assets/Scripts/TextBoxManager.cs
+++ Fading Light/Assets/Scripts/TextBoxManager.cs
@@ -4,49 +4,49 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 public class TextBoxManager : MonoBehaviour {
 
-	public GameObject textBox;
-	public Text dialogue;
-	public TextAsset textFile;
-	public string[] textLines;
-	private string[] splitText;
-	public Image characterImage;
-	public Text characterName;
-	public Sprite[] images;
-	private Dictionary<string,Sprite> myDictionary = new Dictionary<string,Sprite>();
-	private Dictionary<string,string> characterNames = new Dictionary<string,string>();
-	public int currentLine;
-	public int endAtLine;
-	public PlayerController player1;
-	public Player2Controller player2;
+	public GameObject TextBox;
+	public Text Dialogue;
+	public TextAsset TextFile;
+	public string[] TextLines;
+	private string[] _splitText;
+	public Image CharacterImage;
+	public Text CharacterName;
+	public Sprite[] Images;
+	private Dictionary<string,Sprite> SpriteDictionary = new Dictionary<string,Sprite>();
+	private Dictionary<string,string> CharacterNameDictionairy = new Dictionary<string,string>();
+	public int CurrentLine;
+	public int EndLine;
+	public PlayerController Player1;
+	public Player2Controller Player2;
 
-	public bool stopMovement;
-	public bool isActive;
-	public bool firstline;
+	public bool StopMovement;
+	public bool IsActive;
+	public bool FirstLine;
 
-	private bool isTyping = false;
-	private bool cancelTyping = false;
+	private bool _isTyping = false;
+	private bool _cancelTyping = false;
 
-	public AudioClip typeSound;
-	private AudioSource source;
+	public AudioClip TypeSound;
+	private AudioSource _source;
 
-	public float typeSpeed;
+	public float TypeSpeed;
 	// Use this for initialization
 	void Start () {
-		myDictionary.Add ("MM", images [0]);
-		myDictionary.Add ("BS", images [1]);
-		myDictionary.Add ("LS", images [2]);
-		characterNames.Add ("MM", "Mole Man");
-		characterNames.Add ("BS", "Big Sibling");
-		characterNames.Add ("LS", "Little Sibling");
-		if (textFile != null) {
-			textLines = (textFile.text.Split ('\n'));	
+		SpriteDictionary.Add ("MM", Images [0]);
+		SpriteDictionary.Add ("BS", Images [1]);
+		SpriteDictionary.Add ("LS", Images [2]);
+		CharacterNameDictionairy.Add ("MM", "Mole Man");
+		CharacterNameDictionairy.Add ("BS", "Big Sibling");
+		CharacterNameDictionairy.Add ("LS", "Little Sibling");
+		if (TextFile != null) {
+			TextLines = (TextFile.text.Split ('\n'));	
 		}
 
-		if (endAtLine == 0) {
-			endAtLine = textLines.Length - 1;
+		if (EndLine == 0) {
+			EndLine = TextLines.Length - 1;
 		}
 
-		if (isActive) {
+		if (IsActive) {
 			EnableDialogue ();
 		} else {
 			DisableDialogue ();
@@ -55,43 +55,43 @@ public class TextBoxManager : MonoBehaviour {
 	}
 
 	void Awake(){
-		source = GetComponent<AudioSource>();
+		_source = GetComponent<AudioSource>();
 	}
 	// Update is called once per frame
 	void Update () {
-		if (!isActive) {
+		if (!IsActive) {
 			return;
 		}
 			
 
-		if (firstline) {
-			splitText= new string[2];
-			splitText = textLines [currentLine].Split (':');
-			characterImage.sprite = myDictionary [splitText [0]];
-			characterName.text = characterNames [splitText [0]];
+		if (FirstLine) {
+			_splitText= new string[2];
+			_splitText = TextLines [CurrentLine].Split (':');
+			CharacterImage.sprite = SpriteDictionary [_splitText [0]];
+			CharacterName.text = CharacterNameDictionairy [_splitText [0]];
 			//StartCoroutine (TextScroll (textLines [currentLine]));
-			StartCoroutine (TextScroll (splitText[1]));
-			firstline = false;
+			StartCoroutine (TextScroll (_splitText[1]));
+			FirstLine = false;
 		}
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			if (!isTyping) {
-				currentLine += 1;
+			if (!_isTyping) {
+				CurrentLine += 1;
 
-				if (currentLine > endAtLine) {
+				if (CurrentLine > EndLine) {
 					DisableDialogue ();
 				} else {
-					splitText= new string[2];
-					splitText = textLines [currentLine].Split (':');
-					characterImage.sprite = myDictionary [splitText [0]];
-					characterName.text = characterNames [splitText [0]];
-					StartCoroutine (TextScroll (splitText[1]));
+					_splitText= new string[2];
+					_splitText = TextLines [CurrentLine].Split (':');
+					CharacterImage.sprite = SpriteDictionary [_splitText [0]];
+					CharacterName.text = CharacterNameDictionairy [_splitText [0]];
+					StartCoroutine (TextScroll (_splitText[1]));
 					//StartCoroutine (TextScroll (textLines [currentLine]));
 					//dialogue.text = textLines [currentLine];
 				}
 
-			} else if (isTyping && !cancelTyping) {
-				cancelTyping = true;
+			} else if (_isTyping && !_cancelTyping) {
+				_cancelTyping = true;
 			}
 				
 		} 
@@ -103,41 +103,41 @@ public class TextBoxManager : MonoBehaviour {
 
 	private IEnumerator TextScroll (string lineOfText){
 		int letter = 0;
-		dialogue.text = "";
-		isTyping = true;
-		cancelTyping = false;
-		while (isTyping && !cancelTyping && (letter < lineOfText.Length - 1)) {
+		Dialogue.text = "";
+		_isTyping = true;
+		_cancelTyping = false;
+		while (_isTyping && !_cancelTyping && (letter < lineOfText.Length - 1)) {
 			
-			dialogue.text += lineOfText [letter];
-			source.PlayOneShot (typeSound);
+			Dialogue.text += lineOfText [letter];
+			_source.PlayOneShot (TypeSound);
 			letter += 1;
-			yield return new WaitForSeconds (typeSpeed);
+			yield return new WaitForSeconds (TypeSpeed);
 		}
-		dialogue.text = lineOfText;
+		Dialogue.text = lineOfText;
 
-		isTyping = false;
-		cancelTyping = false;
+		_isTyping = false;
+		_cancelTyping = false;
 	}
 
 	public void EnableDialogue(){
-		textBox.SetActive (true);
-		isActive = true;
-			player1.IsDisabled = true;
-			player2.IsDisabled = true;
+		TextBox.SetActive (true);
+		IsActive = true;
+			Player1.IsDisabled = true;
+			Player2.IsDisabled = true;
 	}
 
 	public void DisableDialogue(){
-		textBox.SetActive (false);
-		isActive = false;
-		player1.IsDisabled = false;
-		player2.IsDisabled = false;
+		TextBox.SetActive (false);
+		IsActive = false;
+		Player1.IsDisabled = false;
+		Player2.IsDisabled = false;
 	}
 
 	public void ReloadScript(TextAsset thisText){
 		if (thisText != null) {
-			textLines = new string[1];
-			textLines = (thisText.text.Split ('\n'));	
-			firstline = true;
+			TextLines = new string[1];
+			TextLines = (thisText.text.Split ('\n'));	
+			FirstLine = true;
 
 		}
 	}
