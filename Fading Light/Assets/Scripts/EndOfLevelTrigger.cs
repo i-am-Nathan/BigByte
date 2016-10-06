877fb444ab400a975470fc07c96f750cf9615102
Jack Barker <jack.barker@hotmail.com>
2016-10-06 22:45:21 +1300

    Added more storyline

---
+++ Fading Light/Assets/Scripts/EndOfLevelTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndOfLevelTrigger : MonoBehaviour {
+
+    public Canvas LevelFinishedMenu;
+
+    void Start()
+    {
+        LevelFinishedMenu.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player" || c.gameObject.tag == "Player2")
+        {
+            // Pauses the game and shows the menu indicating that players have completed the level
+            Time.timeScale = 0;
+            LevelFinishedMenu.enabled = true;
+        }
+        
+    }
+}
