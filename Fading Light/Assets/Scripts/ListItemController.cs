c1c7d1b878b1128f599e3ddd1aff360de358cc67
Ammar Bagasrawala <ammarb27@gmail.com>
2016-10-07 03:03:46 +1300

    Added comments to ListItemController

--- Fading Light/Assets/Scripts/ListItemController.cs
+++ Fading Light/Assets/Scripts/ListItemController.cs
@@ -2,6 +2,9 @@
 using UnityEngine.UI;
 using System.Collections;
 
+/// <summary>
+/// To be used in future builds for controlling items players can obtain from a shop
+/// </summary>
 public class ListItemController : MonoBehaviour {
 
 	public Image Icon;
