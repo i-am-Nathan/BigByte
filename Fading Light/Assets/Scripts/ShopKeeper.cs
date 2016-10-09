81ec93240d980d6f577b0312e2aa1cd30e9293e2
Jun Hao Xu <junhaoxu13@hotmail.com>
2016-10-10 00:46:09 +1300

    Adjusted gold coin size, droprate and positioning and started working on shopsystem by creating transition scene when you talk to shopkeeper

---
+++ Fading Light/Assets/Scripts/ShopKeeper.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopKeeper : MonoBehaviour {
+
+	public Camera MainCamera;
+	public Camera ShopKeeperCamera;
+	private bool _shopping;
+	void Awake(){
+		ShopKeeperCamera.enabled = false;
+	}
+		
+	void OnTriggerEnter(Collider other){
+		if (other.name == "Player 1" || other.name == "Player2") {
+			if (Input.GetKeyDown (KeyCode.T) && !_shopping) {
+				MainCamera.enabled = false;
+				ShopKeeperCamera.enabled = true;
+				ShopKeeperCamera.GetComponent<Animation> ().Play ();
+				_shopping = true;
+			}else if (Input.GetKeyDown (KeyCode.T) && _shopping) {
+				MainCamera.enabled = true;
+				ShopKeeperCamera.enabled = false;
+				_shopping = false;
+			}
+		}
+	}
+
+	/// <summary>
+	/// When players press T when they are collding with the chest, it will open it.
+	/// </summary>
+	/// <param name="other">Other.</param>
+	void OnTriggerStay(Collider other){
+		if (other.name == "Player 1" || other.name == "Player2") {
+			if (Input.GetKeyDown (KeyCode.T) && !_shopping) {
+				MainCamera.enabled = false;
+				ShopKeeperCamera.enabled = true;
+				ShopKeeperCamera.GetComponent<Animation> ().Play ();
+				_shopping = true;
+			}else if (Input.GetKeyDown (KeyCode.T) && _shopping) {
+				MainCamera.enabled = true;
+				ShopKeeperCamera.enabled = false;
+				_shopping = false;
+			}
+		}
+
+}
+}
