94e606d4edebfb11106dcac54585d70246ffd92f
Jun Hao Xu <junhaoxu13@hotmail.com>
2016-10-10 01:49:11 +1300

    Enabled action when shopkeeper transition finished

--- Fading Light/Assets/Scripts/ShopKeeper.cs
+++ Fading Light/Assets/Scripts/ShopKeeper.cs
@@ -1,20 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ShopKeeper : MonoBehaviour {
-
+	public GameObject TextBox;
 	public Camera MainCamera;
 	public Camera ShopKeeperCamera;
 	private bool _shopping;
 	private Animation _transition;
 	public PlayerController Player1;
 	public Player2Controller Player2;
+	private bool _hasPlayed;
 	void Awake(){
 		ShopKeeperCamera.enabled = false;
 		_transition = ShopKeeperCamera.GetComponent<Animation> ();
-
 	}
 		
+	void Start(){
+		_hasPlayed = false;
+		TextBox.SetActive (false);
+
+	}
 	void OnTriggerEnter(Collider other){
 		if (other.name == "Player 1" || other.name == "Player2") {
 			if (Input.GetKeyDown (KeyCode.T) && !_shopping) {
@@ -24,7 +30,7 @@ public class ShopKeeper : MonoBehaviour {
 				ShopKeeperCamera.enabled = true;
 				_transition.Play ();
 				_shopping = true;
-
+				_hasPlayed = true;
 
 			}else if (Input.GetKeyDown (KeyCode.T) && _shopping) {
 				MainCamera.enabled = true;
@@ -32,10 +38,17 @@ public class ShopKeeper : MonoBehaviour {
 				_shopping = false;
 				Player1.IsDisabled = false;
 				Player2.IsDisabled = false;
+				TextBox.SetActive(false);
 			}
 		}
 	}
 
+	void Update(){
+		if (!_transition.isPlaying && _hasPlayed) {
+			TextBox.SetActive(true);
+		}
+
+	}
 	/// <summary>
 	/// When players press T when they are collding with the chest, it will open it.
 	/// </summary>
@@ -49,6 +62,8 @@ public class ShopKeeper : MonoBehaviour {
 				ShopKeeperCamera.enabled = true;
 				_transition.Play ();
 				_shopping = true;
+				_hasPlayed = true;
+
 
 
 			}else if (Input.GetKeyDown (KeyCode.T) && _shopping) {
@@ -57,6 +72,7 @@ public class ShopKeeper : MonoBehaviour {
 				_shopping = false;
 				Player1.IsDisabled = false;
 				Player2.IsDisabled = false;
+				TextBox.SetActive(false);
 			}
 		}
 	}
