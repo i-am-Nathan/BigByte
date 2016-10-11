e528bf67d456131628586e31741028f1e570698c
AdiJNair <adijnair@gmail.com>
2016-10-11 13:02:39 +1300

    axe trap lever

---
+++ Fading Light/Assets/Scripts/TrapScripts/LeverTraps.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverTraps : MonoBehaviour {
+    public GameObject[] gameObjects;
+    private bool _pressed = false;
+
+    // Use this for initialization
+    void Start () {
+        foreach (GameObject obj in gameObjects)
+        {
+            if (obj.tag.Equals("SawTrap"))
+            {
+                obj.GetComponent<Animation>().Stop();
+            }
+            if
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
