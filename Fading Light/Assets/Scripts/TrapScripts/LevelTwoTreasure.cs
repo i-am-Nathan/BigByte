d3add2021c4b978cfee27a8f488370d948011d57
AdiJNair <adijnair@gmail.com>
2016-10-13 19:03:27 +1300

    added traps in level 2

--- Fading Light/Assets/Scripts/TrapScripts/LevelTwoTreasure.cs
+++ Fading Light/Assets/Scripts/TrapScripts/LevelTwoTreasure.cs
@@ -1,25 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LevelTwoTreasure : MonoBehaviour {
+public class LevelTwoTreasure : MonoBehaviour
+{
 
-    public GameObject[] spikeTrap;
+    public GameObject[] hammerTrap;
     public GameObject[] spearTrap;
+    private bool _pulled = false;
 
     // Use this for initialization
-    void Start () {
-        foreach (GameObject obj in spikeTrap)
+    void Start()
+    {
+        foreach (GameObject obj in hammerTrap)
         {
-            obj.GetComponent<Animation>().Play("Anim_TrapNeedle_Hide");
+            obj.GetComponent<Animation>().Play();
         }
         foreach (GameObject obj2 in spearTrap)
         {
-            obj2.GetComponent<Animation>().Stop();
+            obj2.GetComponent<Animation>().Play();
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Called when the player is close enough to the lever, and presses T
+    /// </summary>
+    void OnTriggerStay(Collider other)
+    {
+        //if T is pressed to interact with the lever, the walls move
+        if (Input.GetKeyDown(KeyCode.T) && !_pulled)
+        {
+            this.GetComponent<Animation>().Play("Armature|LeverDown");
+            foreach (GameObject obj in hammerTrap)
+            {
+                Destroy(obj);
+            }
+            foreach (GameObject obj2 in spearTrap)
+            {
+
+                Destroy(obj2);
+            }
+            _pulled = true;
+        }
+    }
 }
