5a5e82f9628b46a28b56b89ba967f8d3736f607f
AdiJNair <adijnair@gmail.com>
2016-10-13 18:11:19 +1300

    fixed prefabs so player 2 can attack

---
+++ Fading Light/Assets/Scripts/TrapScripts/LevelTwoTreasure.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTwoTreasure : MonoBehaviour {
+
+    public GameObject[] spikeTrap;
+    public GameObject[] spearTrap;
+
+    // Use this for initialization
+    void Start () {
+        foreach (GameObject obj in spikeTrap)
+        {
+            obj.GetComponent<Animation>().Play("Anim_TrapNeedle_Hide");
+        }
+        foreach (GameObject obj2 in spearTrap)
+        {
+            obj2.GetComponent<Animation>().Stop();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
