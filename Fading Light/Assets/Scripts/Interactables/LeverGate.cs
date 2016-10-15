74b54a066ed839b4952321a049787ab9a574cd86
unknown <jjin525@EN-284807.UoA.auckland.ac.nz>
2016-10-15 15:43:11 +1300

    added falling spear gate with lever and animation.

---
+++ Fading Light/Assets/Scripts/Interactables/LeverGate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// This script controls the levers animations and triggers as well as the animations of the moving walls
+/// </summary>
+public class LeverGate : MonoBehaviour
+{
+    private bool _pulled = false;
+
+    /// <summary>
+    /// Called when the player is close enough to the lever, and presses T
+    /// </summary>
+    void OnTriggerStay(Collider other)
+    {
+        //if T is pressed to interact with the lever, the walls move
+        if (Input.GetKeyDown(KeyCode.T) && !_pulled)
+        {
+            this.GetComponent<Animation>().Play("Armature|LeverDown");
+
+            Transform spearWall = transform.parent.transform.Find("spear_wall");
+
+            foreach (Transform spearBlock in spearWall)
+                spearBlock.transform.Find("Spear").GetComponent<Animation>().Play("Spear_Fall");
+
+            _pulled = true;
+        }
+    }
+}
