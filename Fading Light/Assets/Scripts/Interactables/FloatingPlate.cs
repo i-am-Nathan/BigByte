1c0c9d56b105a565afd1281a506360d28298f56c
Jun Hao Xu <junhaoxu13@hotmail.com>
2016-10-15 13:29:01 +1300

    Allowed players to stay on floating platform

--- Fading Light/Assets/Scripts/Interactables/FloatingPlate.cs
+++ Fading Light/Assets/Scripts/Interactables/FloatingPlate.cs
@@ -3,57 +3,57 @@ using System.Collections;
 
 public class FloatingPlate : MonoBehaviour {
 
-    public GameObject _backBumper;
-    public GameObject _frontBumper;
+
+	public float TravelSpeed;
     private bool _goingUp = true;
     private float _velocity = 50;
+	private int _direction =1 ;
+	private bool _pressedDown = false;
 
+
+	void Start(){
+		_direction = 1;
+	}
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag.Equals("player") || other.tag.Equals("player2"))
+		if (other.name == "Player 1" || other.name == "Player2") 
         {
 
-            other.gameObject.transform.parent = this.transform;
-            Debug.LogWarning(other.gameObject.transform.parent);
+            other.transform.parent = transform;
 
-        } else if (other.gameObject.Equals(_backBumper))
+		} else if (other.name == "Start" || other.name== "End")
         {
-            _goingUp = true;
-            GetComponent<Rigidbody>().velocity = new Vector3(_velocity * -1, 0f, 0f);
+			Debug.Log ("In HERE");
+			if (_direction == 1) {
+				_direction = -1;
+			} else {
+				_direction = 1;
+			}
 
-        } else if (other.gameObject.Equals(_frontBumper))
-        {
-            _goingUp = false;
-            GetComponent<Rigidbody>().velocity = new Vector3(_velocity, 0f, 0f);
-        }
+		} 
     }
 
     public void Resume()
     {
-        if (_goingUp)
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(_velocity * -1, 0f, 0f);
-        }
-        else
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(_velocity, 0f, 0f);
-        }
-
-        
+		_pressedDown = true;
     }
 
     public void Stop()
     {
-            GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+		_pressedDown = false;
     }
 
-
+	void FixedUpdate(){
+		if (_pressedDown) {
+			transform.Translate (new Vector3(1,0,0)* TravelSpeed * _direction * Time.deltaTime);
+		}
+	}
 
     void OnTriggerExit(Collider other)
     {
-        if (other.tag.Equals("player") || other.tag.Equals("player2"))
-        {
-            other.gameObject.transform.parent = null;
+		if (other.name == "Player 1" || other.name == "Player2") {
+        
+            other.transform.parent = null;
 
         }
     }
