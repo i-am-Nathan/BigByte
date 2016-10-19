696dfebd195f548f5387e6ceeffdbefd554903c1
Jun Hao Xu <junhaoxu13@hotmail.com>
2016-10-20 00:59:01 +1300

    Fixed minor issue with players being able to set plate again after all spike traps disabled

--- Fading Light/Assets/Scripts/Interactables/UnsetTraps.cs
+++ Fading Light/Assets/Scripts/Interactables/UnsetTraps.cs
@@ -4,22 +4,31 @@ using System.Collections.Generic;
 
 
 /// <summary>
-/// This script controls the pressure plate and falling wall's trigger animations
+/// This script controls the spiked traps and unsets all the traps when the player reaches it
 /// </summary>
 public class UnsetTraps : MonoBehaviour {
 
 	private GameObject[] _otherPlates;
 	private bool _pressed=false;
 
+	/// <summary>
+	/// Gets a reference to all the traps.
+	/// </summary>
 	void Awake(){
 		_otherPlates = GameObject.FindGameObjectsWithTag ("SpearTrap");
 	}
+	/// <summary>
+	/// When a player enters the plate it will unset all the needles and disable the plates.
+	/// </summary>
+	/// <param name="other">Other.</param>
 	void OnTriggerEnter(Collider other) {
 		if (other.name == "Player 1" || other.name == "Player2") {
 			if (!_pressed){
 			gameObject.GetComponent<Animation> ().Play ("PressurePlateDown");
+
 				_pressed = true;
 				foreach (GameObject o in _otherPlates) {
+					o.GetComponent<TrapPlate> ().Disabled = true;
 				o.GetComponent<TrapPlate> ().UnsetTraps ();
 			}
 			}
