5e9e5dee7f327668d962706a839a83ea41d5ed4d
Jack Barker <jack.barker@hotmail.com>
2016-10-20 19:29:05 +1300

    Started boss cutscene for level 2

---
+++ Fading Light/Assets/CutSceneTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutSceneTrigger : MonoBehaviour {
+
+
+    public Storyline Story;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Story.Next();
+    }
+}
