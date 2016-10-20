4e582f706038dc883d55dbda0fd5b7cbc56b4ad6
Jack Barker <jack.barker@hotmail.com>
2016-10-20 22:36:37 +1300

    Started level 4 cut scene

---
+++ Fading Light/Assets/Scripts/GameControl/Storyline_Level4.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Storyline_Level4 : Storyline
+{
+
+
+    public PlayerController Player1;
+    public Player2Controller Player2;
+    public List<MoleManContoller> MoleMen;
+    public TorchFuelController TorchController;
+    public List<GameObject> ReferencePoints;
+    public GameObject CameraRig;
+    public MoleManContoller SmallMoleMan;
+    public List<GameObject> CutScenePositions;
+    public List<GameObject> CustSceneTargets;
+
+    public int _currentStep = 0;
+    private bool _done = false;
+    private ToolTips _tips;
+    private bool _tipsDone = false;
+    private float _startDisplay;
+
+    public override void DialogueComplete()
+    {
+        _currentStep++;
+        _done = false;
+       // throw new NotImplementedException();
+    }
+
+    public override void EnableMoleMan()
+    {
+        //throw new NotImplementedException();
+    }
+
+    public override void Next()
+    {
+        if(_currentStep == 4)
+        {
+            _currentStep++;
+            _done = false;
+        }
+       
+    }
+
+    public override void NextMoleMan()
+    {
+        if (_currentStep == 1)
+        {
+            _currentStep++;
+            _done = false;
+        }
+        //throw new NotImplementedException();
+    }
+
+    public override void StartText()
+    {
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_done)
+        {
+            return;
+        }
+
+        if (_currentStep == 0)
+        {
+            SmallMoleMan.IsDisabled = true;
+            TorchController.SwapPlayers();
+            //zoom in on prisoner
+            CameraRig.GetComponent<PlayerCam>().SwoopPositionTarget = CutScenePositions[0];
+            CameraRig.GetComponent<PlayerCam>().SwoopAngleTarget = CustSceneTargets[0];
+            CameraRig.GetComponent<PlayerCam>().CameraState = 1;
+            TorchController.IsDisabled = true;
+            //Moleman walking to players
+            _done = true;
+            Player1.IsDisabled = true;
+            Player2.IsDisabled = true;
+            _done = true;
+        }
+        else if (_currentStep == 1)
+        {
+            //Talking done
+            CameraRig.GetComponent<PlayerCam>().CameraState = 0;
+            Player1.IsDisabled = false;
+            Player2.IsDisabled = false;
+            _done = true;
+        }
+        else if (_currentStep == 2)
+        {
+            //Transformation done
+            CameraRig.GetComponent<PlayerCam>().CameraState = 0;
+            _done = true;
+        }
+    }
+
+    public override void CharacterDamageEnabled(bool enabled)
+    {
+        Player1.CanTakeDamage = enabled;
+        Player2.CanTakeDamage = enabled;
+    }
+}
